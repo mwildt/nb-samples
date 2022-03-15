381e9ec9b49b3cc166f9afbd86bdaa63f31b27dd
Malte Wildt <malte.wildt@nrwbank.de>
2022-03-15 14:51:32 +0100

    remove EXPOSE statmenet for specific port 80

--- main.go
+++ main.go
@@ -6,10 +6,19 @@ import (
 	"net/http"
 )
 
+func requestLogger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s", r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 
+	log.Printf("Running NB Sample Web-Application")
+
 	fs := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fs)
+	http.Handle("/", requestLogger(fs))
 
 	serviceHost, ok := os.LookupEnv("SERVICE_HOST")
 
